8bb74a72e05b1c305102a6e849e8d85bc0ed04a5
Patryk Wisniewski <patrykwis11@hotmail.com>
2023-03-31 16:07:07 +0200

    Zadanie z 3 dnia

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
-ConsoleColor(bool)
+﻿string name = "Ewa";
+string sex = "Women";
+int age = 33;
+
+if (age == 33)
+{
+    if (name == "Ewa")
+    if (sex == "Women")
+    Console.WriteLine("Ewa, lat 33");
+}
+
+else
+{
+    if (name == "Ewa")
+    if (age < 33)
+    Console.WriteLine("Ewa, lat 45");
+
+}
