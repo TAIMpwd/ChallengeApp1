0a474d6f28a9d506ac4ebbfc7c474d4aed26458f
Patryk Wisniewski <patrykwis11@hotmail.com>
2023-03-31 16:27:58 +0200

    Zadanie z dnia 4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -2,17 +2,13 @@
 string sex = "Women";
 int age = 33;
 
-if (age == 33)
-{
-    if (name == "Ewa")
-    if (sex == "Women")
-    Console.WriteLine("Ewa, lat 33");
-}
+if (age == 33 && name == "Ewa" && sex == "Women")
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
 
-else
-{
-    if (name == "Ewa")
-    if (age < 33)
+    else
+    if (name == "Ewa" && age < 33)
+{   
     Console.WriteLine("Ewa, lat 45");
-
 }
