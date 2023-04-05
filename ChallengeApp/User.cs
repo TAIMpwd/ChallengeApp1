dbb1113c01875002cdd61adee59e75202c3094d7
Patryk Wisniewski <patrykwis11@hotmail.com>
2023-04-05 18:35:39 +0200

    Zadanie dzien 6

---
+++ ChallengeApp/User.cs
@@ -0,0 +1,35 @@
+﻿namespace ChallengeApp
+{
+    public class Employee
+    {
+
+        private List<int> Score = new List<int>();
+
+
+        public Employee(string Name, string Surname, int Age)
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.Age = Age;
+        }
+
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set; }
+
+        public int Result
+        {
+            get
+            {
+                return this.Score.Sum();
+            }
+
+        }
+
+
+        public void AddScore(int number)
+        {
+            this.Score.Add(number);
+        }
+    }
+}
