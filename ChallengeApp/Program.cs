49af977ded7c69f93e3363c56108d5fc7597abce
Patryk Wisniewski <patrykwis11@hotmail.com>
2023-04-05 18:35:27 +0200

    Zadanie Dzien 6

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,71 +1,43 @@
-﻿int number = 4456;
-String numberInString = number.ToString();
-char[] letters = numberInString.ToCharArray();
+﻿using ChallengeApp;
 
-Console.WriteLine("Ilość cyfr danych cyfr w:");
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
+List<Employee> employees = new List<Employee>();
 
-foreach (char item in letters)
+Employee employee1 = new Employee("Patryk", "Wisniewski", 26);
+Employee employee2 = new Employee("Staszek", "Kolasa", 41);
+Employee employee3 = new Employee("Grzegorz", "Brzęczyszczykiewicz", 45);
+
+employee1.AddScore(9);
+employee1.AddScore(6);
+employee1.AddScore(8);
+employee1.AddScore(7);
+employee1.AddScore(1);
+
+employee2.AddScore(7);
+employee2.AddScore(5);
+employee2.AddScore(8);
+employee2.AddScore(9);
+employee2.AddScore(6);
+
+employee3.AddScore(4);
+employee3.AddScore(7);
+employee3.AddScore(8);
+employee3.AddScore(4);
+employee3.AddScore(6);
+
+employees.Add(employee1);
+employees.Add(employee2);
+employees.Add(employee3);
+
+int MaxResult = 0;
+Employee employeeWithMaxResult = null;
+foreach (var employee in employees)
 {
-    if (item == '0')
-    {
-        counter0++;
+    if (employee.Result > MaxResult)
+    { MaxResult = employee.Result;
+      employeeWithMaxResult = employee;      
     }
-    else if (item == '1')
-    {
-        counter1++;
-    }
-    else if (item == '2')
-    {
-        counter2++;
-    }
-    else if (item == '3')
-    {
-        counter3++;
-    }
-    else if (item == '4')
-    {
-        counter4++;
-    }
-    else if (item == '5')
-    {
-        counter5++;
-    }
-    else if (item == '6')
-    {
-        counter6++;
-    }
-    else if (item == '7')
-    {
-        counter7++;
-    }
-    else if (item == '8')
-    {
-        counter8++;
-    }
-    else if (item == '9')
-    {
-        counter9++;
-    }
-
-    
 }
-Console.WriteLine("Cyfr 0 jest:" + counter0);
-Console.WriteLine("Cyfr 1 jest:" + counter1);
-Console.WriteLine("Cyfr 2 jest:" + counter2);
-Console.WriteLine("Cyfr 3 jest:" + counter3);
-Console.WriteLine("Cyfr 4 jest:" + counter4);
-Console.WriteLine("Cyfr 5 jest:" + counter5);
-Console.WriteLine("Cyfr 6 jest:" + counter6);
-Console.WriteLine("Cyfr 7 jest:" + counter7);
-Console.WriteLine("Cyfr 8 jest:" + counter8);
-Console.WriteLine("Cyfr 9 jest:" + counter9);
+Console.WriteLine("Najwiecej punków zdobył");
+Console.WriteLine(employeeWithMaxResult.Name + " " +  employeeWithMaxResult.Surname);
+Console.WriteLine("Wiek" + employeeWithMaxResult.Age);
+Console.WriteLine("z wynikiem" + employeeWithMaxResult.Result + "punktów");
