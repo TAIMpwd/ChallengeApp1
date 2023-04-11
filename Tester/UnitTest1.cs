5e9fff147e8a1b573074d11f9d498603f2b70ebf
Patryk Wisniewski <patrykwis11@hotmail.com>
2023-04-11 12:52:08 +0200

    Tester Dzien 7

---
+++ Tester/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Tester
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
