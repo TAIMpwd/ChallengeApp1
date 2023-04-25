1f0d12c48011728718fb6d382baece9cbffd31af
Patryk Wisniewski <patrykwis11@hotmail.com>
2023-04-25 11:53:49 +0200

    Zadanie dzien 10

---
+++ ChallengeApp/Employee.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ChallengeApp
+{
+    public class Employee
+    {
+        private List<float> grades = new List<float>();
+        public Employee(string name, string surname)
+        {
+            this.Name = name;
+            this.Surname = surname;
+        }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public void AddGrade(float grade)
+        {
+            if (grade >= 0 && grade <= 100)
+            {
+                this.grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("Invalid grade value ");
+            }
+        }
+        public void AddGrade(string grade)
+        {
+            if (float.TryParse(grade, out float result))
+            {
+                this.AddGrade(result);
+            }
+            else
+            {
+                Console.WriteLine("String is not a float");
+            }
+        }
+        public void AddGrade(double grade)
+        {
+            float gradeAsFloat = (float)grade;
+            this.AddGrade(gradeAsFloat);
+        }
+
+        public Statistic GetStatistic()
+        {
+            var statistic = new Statistic();
+            statistic.Avarage = 0;
+            statistic.Max = float.MinValue;
+            statistic.Min = float.MaxValue;
+
+            foreach (var grade in this.grades)
+            {
+                statistic.Max = Math.Max(statistic.Max, grade);
+                statistic.Min = Math.Min(statistic.Min, grade);
+                statistic.Avarage += grade;
+            }
+
+            statistic.Avarage /= this.grades.Count;
+            return statistic;
+        }
+    }
+}
