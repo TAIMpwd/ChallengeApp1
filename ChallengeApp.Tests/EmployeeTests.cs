d04c2c0719429212a2bc0e59cc727aeea7a824d3
Patryk Wisniewski <patrykwis11@hotmail.com>
2023-04-25 09:43:24 +0200

    EmployeeTest Dzień 9

---
+++ ChallengeApp.Tests/EmployeeTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+
+namespace ChallengeApp.Tests
+{
+    public class EmployeeTests
+    {
+        [Test]
+        public void WhenGetStatisticsCalled_ShouldRetournCorrectMax()
+        {
+            //Arrange
+            var employee = new Employee("Patryk", "Wisniewski");
+            employee.AddGrade(2);
+            employee.AddGrade(2);
+            employee.AddGrade(6);
+
+            //Act
+            var statistics = employee.GetStatistic();
+
+            //Assert
+            Assert.AreEqual(6, statistics.Max);
+
+        }
+        [Test]
+        public void WhenGetStatisticsCalled_ShouldRetournCorrectMin()
+        {
+            //Arrange
+            var employee = new Employee("Patryk", "Wisniewski");
+            employee.AddGrade(2);
+            employee.AddGrade(2);
+            employee.AddGrade(6);
+
+            //Act
+            var statistics = employee.GetStatistic();
+
+            //Assert
+            Assert.AreEqual(2, statistics.Min);
+
+        }
+        [Test]
+        public void WhenGetStatisticsCalled_ShouldRetournCorrectAverage()
+        {
+            //Arrange
+            var employee = new Employee("Patryk", "Wisniewski");
+            employee.AddGrade(2);
+            employee.AddGrade(2);
+            employee.AddGrade(6);
+
+            //Act
+            var statistics = employee.GetStatistic();
+
+            //Assert
+            Assert.AreEqual(Math.Round(3.33, 2), Math.Round(statistics.Avarage, 2));
+
+        }
+
+    }
+}
